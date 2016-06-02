b7e2d60608f287bb17bd216550b3da8b670be96e
Koren <koren.hmel@gmail.com>
2016-06-02 16:43:11 +0300

    create database and add .gitignore

--- app.rb
+++ app.rb
@@ -1,7 +1,20 @@
 #encoding: utf-8
 require 'rubygems'
 require 'sinatra'
+require 'sqlite3'
 # require "sinatra/reloader"
+def init_db
+  @db = SQLite3::Database.new 'leprozorium.db'
+  @db.results_as_hash = true
+end
+configure do
+    init_db
+  @db.execute 'CREATE  TABLE IF NOT EXISTS Posts
+ (
+id INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL ,
+ created_date DATETIME,
+ content TEXT)'
+end
 
 get '/' do
 	erb "Hello!!!! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School!!!</a>"
@@ -9,4 +22,9 @@ end
 
 get '/new' do
   erb :new
+end
+
+post '/new' do
+name =  params[:post]
+  erb name
 end
